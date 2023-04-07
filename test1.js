8518e1ec51d311dcc9898273730e603171b124e9
kabakovIlya <ilya.08ka@gmail.com>
2023-04-07 08:38:51 +0300

    upd2.0

--- test1.js
+++ test1.js
@@ -1,23 +1,2 @@
 const test1 = "test1";
-alert("Привіт!")
-
-fbdfdxfhd
-gbc
-gsggfvsdgb
-fdbdbd
-dfv
-dfdf
-dfvvdf
-dfdv
-dfdv
-dfdv
-ddffv
-fvfv
-h
-fgb
-fgb
-fgb
-fgb
-fgbf
-s
-blablablabla
+alert("Привіт
